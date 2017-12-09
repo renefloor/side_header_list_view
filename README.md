928f92e1d65eb68f922ac54401d8f319d992599d
Rene Floor <r.floor.1@gmail.com>
2017-12-09 15:08:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# side_header_list_view
+
+A new flutter package project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](http://flutter.io/).
+
+For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
---
+++ lib/side_header_list_view.dart
@@ -0,0 +1,108 @@
+library side_header_list_view;
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+
+/**
+ *  SideHeaderListView for Flutter
+ *
+ *  Copyright (c) 2017 Rene Floor
+ *
+ *  Released under BSD License.
+ */
+
+
+typedef bool HasSameHeader(int a, int b);
+
+class SideHeaderListView extends StatefulWidget {
+  final int itemCount;
+  final IndexedWidgetBuilder headerBuilder;
+  final IndexedWidgetBuilder itemBuilder;
+  final EdgeInsets padding;
+  final HasSameHeader hasSameHeader;
+  final itemExtend;
+
+  SideHeaderListView({
+    Key key,
+    this.itemCount,
+    @required this.itemExtend,
+    @required this.headerBuilder,
+    @required this.itemBuilder,
+    @required this.hasSameHeader,
+    this.padding,
+  })
+      : super(key: key);
+
+  @override
+  _SideHeaderListViewState createState() => new _SideHeaderListViewState();
+}
+
+class _SideHeaderListViewState extends State<SideHeaderListView> {
+  int currentPosition = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new Positioned(
+          child: new Opacity(
+            opacity: _shouldShowHeader(currentPosition) ? 0.0 : 1.0,
+            child: widget.headerBuilder(context, currentPosition),
+          ),
+          top: 0.0,
+          left: 0.0,
+        ),
+        new ListView.builder(
+            padding: widget.padding,
+            itemCount: widget.itemCount,
+            itemExtent: widget.itemExtend,
+            controller: _getScrollController(),
+            itemBuilder: (BuildContext context, int index) {
+              return new Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new FittedBox(
+                    child: new Opacity(
+                      opacity: _shouldShowHeader(index) ? 1.0 : 0.0,
+                      child: widget.headerBuilder(context, index),
+                    ),
+                  ),
+                  new Expanded(child: widget.itemBuilder(context, index))
+                ],
+              );
+            }),
+      ],
+    );
+  }
+
+  bool _shouldShowHeader(int position) {
+    if (position == 0 || position == widget.itemCount - 1) {
+      return false;
+    }
+    if (position != currentPosition &&
+        !widget.hasSameHeader(position, position - 1)) {
+      return true;
+    }
+
+    if (!widget.hasSameHeader(position, position + 1) &&
+        position == currentPosition) {
+      return true;
+    }
+    return false;
+  }
+
+  ScrollController _getScrollController() {
+    var controller = new ScrollController();
+    controller.addListener(() {
+      var pixels = controller.offset;
+      var newPosition = (pixels / widget.itemExtend).floor();
+      if (newPosition != currentPosition) {
+        setState(() {
+          currentPosition = newPosition;
+        });
+      }
+    });
+    return controller;
+  }
+}
